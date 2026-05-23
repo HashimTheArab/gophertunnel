f7fcb8c8b9e69e1980ba25ed846de6b5e49b30dd
Hashim <hashim@lunarbedrock.com>
2026-05-23 01:07:31 +0000

    Revert "minecraft: harden NetherNet network wrapper"
    This reverts commit dad6548b2d9d2d08dadc16a7c4b74d8435e1080d.

--- minecraft/nethernet.go
+++ minecraft/nethernet.go
@@ -11,29 +11,13 @@ import (
 
 // NetherNet is an implementation of a NetherNet network.
 type NetherNet struct {
-	// Signaling is used to exchange WebRTC signals and ICE credentials.
-	// It must be non-nil when dialing or listening.
 	Signaling nethernet.Signaling
-
-	// Log is used for dialing and listening when Dialer.Log or ListenConfig.Log is nil.
-	Log *slog.Logger
-
-	// Dialer specifies options for establishing connections with DialContext.
-	Dialer nethernet.Dialer
-	// ListenConfig specifies options for listening for incoming connections with Listen.
-	ListenConfig nethernet.ListenConfig
+	Log       *slog.Logger
 }
 
 // DialContext ...
 func (n NetherNet) DialContext(ctx context.Context, address string) (net.Conn, error) {
-	if n.Signaling == nil {
-		return nil, errors.New("minecraft: NetherNet.DialContext: Signaling is nil")
-	}
-	d := n.Dialer
-	if d.Log == nil {
-		d.Log = n.Log
-	}
-	return d.DialContext(ctx, address, n.Signaling)
+	return nethernet.Dialer{Log: n.Log}.DialContext(ctx, address, n.Signaling)
 }
 
 // PingContext ...
@@ -43,12 +27,5 @@ func (n NetherNet) PingContext(context.Context, string) ([]byte, error) {
 
 // Listen ...
 func (n NetherNet) Listen(string) (NetworkListener, error) {
-	if n.Signaling == nil {
-		return nil, errors.New("minecraft: NetherNet.Listen: Signaling is nil")
-	}
-	conf := n.ListenConfig
-	if conf.Log == nil {
-		conf.Log = n.Log
-	}
-	return conf.Listen(n.Signaling)
+	return nethernet.ListenConfig{Log: n.Log}.Listen(n.Signaling)
 }
