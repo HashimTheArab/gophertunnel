65adbe8459d6652942979802ce1b237312883194
HashimTheArab <hashimssoftware@gmail.com>
2026-08-28 18:30:40 +0300

    minecraft: include fixed vehicle container sizes

--- minecraft/protocol/container.go
+++ minecraft/protocol/container.go
@@ -130,6 +130,10 @@ func ContainerTypeSlotCount(containerType byte) (int, bool) {
 		return 9, true
 	case ContainerTypeHopper:
 		return 5, true
+	case ContainerTypeCartChest, ContainerTypeChestBoat:
+		return 27, true
+	case ContainerTypeCartHopper:
+		return 5, true
 	case ContainerTypeBeacon, ContainerTypeDecoratedPot:
 		return 1, true
 	case ContainerTypeLoom, ContainerTypeSmithingTable:
