7591fab883f59c3a65da384af5a5689e48015b0a
HashimTheArab <hashimssoftware@gmail.com>
2026-08-28 17:58:17 +0300

    minecraft: expose fixed container screen sizes

--- minecraft/protocol/container.go
+++ minecraft/protocol/container.go
@@ -111,6 +111,34 @@ const (
 	ContainerTypeCrafter
 )
 
+// ContainerTypeSlotCount returns the fixed number of slots exposed by a
+// container screen type. The bool is false for screens whose capacity depends
+// on runtime block, entity or screen state.
+func ContainerTypeSlotCount(containerType byte) (int, bool) {
+	switch containerType {
+	case ContainerTypeWorkbench:
+		return 10, true
+	case ContainerTypeFurnace, ContainerTypeBlastFurnace, ContainerTypeSmoker:
+		return 3, true
+	case ContainerTypeEnchantment, ContainerTypeStonecutter:
+		return 2, true
+	case ContainerTypeBrewingStand:
+		return 5, true
+	case ContainerTypeAnvil, ContainerTypeGrindstone, ContainerTypeCartography:
+		return 3, true
+	case ContainerTypeDispenser, ContainerTypeDropper, ContainerTypeCrafter:
+		return 9, true
+	case ContainerTypeHopper:
+		return 5, true
+	case ContainerTypeBeacon, ContainerTypeDecoratedPot:
+		return 1, true
+	case ContainerTypeLoom, ContainerTypeSmithingTable:
+		return 4, true
+	default:
+		return 0, false
+	}
+}
+
 // FullContainerName contains information required to identify a container in a StackRequestSlotInfo.
 type FullContainerName struct {
 	// ContainerID is the ID of the container that the slot was in.
