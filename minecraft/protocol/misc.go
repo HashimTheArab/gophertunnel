913c58912b1701747319871d54e9d90dfdd8d030
HashimTheArab <hashimssoftware@gmail.com>
2026-09-25 17:52:56 +0300

    protocol: one-byte InventorySlot container ID and start-first PlayerVideoCapture actions in the preview

--- minecraft/protocol/misc.go
+++ minecraft/protocol/misc.go
@@ -1880,7 +1880,7 @@ type StartVideoCapture struct {
 	FilePrefix string
 }
 
-func (*StartVideoCapture) tagPlayerVideoCaptureData() uint8 { return 1 }
+func (*StartVideoCapture) tagPlayerVideoCaptureData() uint32 { return 0 }
 
 // Marshal reads or writes StartVideoCapture using its canonical wire layout.
 func (x *StartVideoCapture) Marshal(io IO) {
@@ -1891,7 +1891,7 @@ func (x *StartVideoCapture) Marshal(io IO) {
 type StopVideoCapture struct {
 }
 
-func (*StopVideoCapture) tagPlayerVideoCaptureData() uint8 { return 0 }
+func (*StopVideoCapture) tagPlayerVideoCaptureData() uint32 { return 1 }
 
 // Marshal reads or writes StopVideoCapture using its canonical wire layout.
 func (x *StopVideoCapture) Marshal(io IO) {
